311ae4021a74115c73f9f663fce17b426b4409dd
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2023-05-12 17:57:29 -0700

    System status error handling

--- systemStatus.js
+++ systemStatus.js
@@ -3,16 +3,22 @@ import { promisify } from "util";
 
 const promiseExec = promisify(exec);
 
-async function getStatusSystemd() {
+export async function getStatusSystemd() {
     try {
         const status = await promiseExec(
             `systemctl list-jobs | egrep -q 'shutdown.target.*start' && echo "shutting down" || echo "not shutting down"; systemctl list-jobs | egrep -q 'reboot.target.*start' && echo "-> rebooting" || echo "-> not rebooting"; systemctl list-jobs | egrep -q 'halt.target.*start' && echo "-> halting" || echo "-> not halting"; systemctl list-jobs | egrep -q 'poweroff.target.*start' && echo "-> powering down" || echo "-> not powering down"`
         );
         return status.stdout;
     } catch (error) {
-        console.error(error);
+        if (error.code === 255) {
+            if (process.platform === "win32") {
+                console.error("Windows doesn't support systemctl.");
+            } else {
+                console.error(error.stderr.trim());
+            }
+        } else {
+            console.error(error);
+        }
     }
     return "unknown";
 }
-
-export default getStatusSystemd;

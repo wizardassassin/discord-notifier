e82e7ab2f4d1f5632a839afb1743e73e2fb30dcc
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2023-06-15 14:59:42 -0700

    Updated events

--- events/mcServerEvents.js
+++ events/mcServerEvents.js
@@ -1,3 +1,4 @@
+import assert from "assert/strict";
 import { EmbedWrapper, WebhookWrapper } from "../webhookTemplates.js";
 
 export const disabled = false;
@@ -11,12 +12,28 @@ export const eventList = [
     { id: "minecraft_leave", execute: playerLeave },
 ];
 
+const getPlayerEmbed = (name) =>
+    EmbedWrapper.genericEmbed().setColor("DarkGreen").setAuthor({
+        name: name,
+        iconURL: "",
+    });
+const getEmbed = (title) => EmbedWrapper.titleEmbed(title).setColor("DarkGreen");
+
 /**
  *
  * @param {{webhook: WebhookWrapper, msg: string}} data Message Data
  */
 async function serverStart({ webhook, msg }) {
-    const embed = EmbedWrapper.minecraftEmbed(msg);
+    const [rawVersion, motd, maxPlayers] = msg.split("\n");
+    const sep = rawVersion.indexOf(" ");
+    assert.ok(sep !== -1, "Couldn't find space seperator.");
+    const mcVersion = rawVersion.slice(sep + 1);
+    const name = rawVersion.slice(0, sep).split("-")[1];
+    const embed = getEmbed("✅ Minecraft Server has Started").setFields(
+        { name: "Version", value: `${name} ${mcVersion}` },
+        { name: "MOTD", value: motd },
+        { name: "Players", value: `0/${maxPlayers}` }
+    );
     await webhook.sendEmbed(embed);
 }
 
@@ -25,7 +42,7 @@ async function serverStart({ webhook, msg }) {
  * @param {{webhook: WebhookWrapper, msg: string}} data Message Data
  */
 async function serverStop({ webhook, msg }) {
-    const embed = EmbedWrapper.minecraftEmbed(msg);
+    const embed = getPlayerEmbed("🛑 Minecraft Server has Stopped");
     await webhook.sendEmbed(embed);
 }
 
@@ -34,7 +51,7 @@ async function serverStop({ webhook, msg }) {
  * @param {{webhook: WebhookWrapper, msg: string}} data Message Data
  */
 async function playerJoin({ webhook, msg }) {
-    const embed = EmbedWrapper.minecraftEmbed(msg);
+    const embed = getEmbed().setDescription(msg);
     await webhook.sendEmbed(embed);
 }
 
@@ -43,6 +60,6 @@ async function playerJoin({ webhook, msg }) {
  * @param {{webhook: WebhookWrapper, msg: string}} data Message Data
  */
 async function playerLeave({ webhook, msg }) {
-    const embed = EmbedWrapper.minecraftEmbed(msg);
+    const embed = getEmbed().setDescription(msg);
     await webhook.sendEmbed(embed);
 }

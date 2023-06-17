e68a6f8f4a7e56de91c7cf29fb40610344ad9ea4
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2023-06-16 20:18:16 -0700

    Update template files

--- events/template.js
+++ events/template.js
@@ -10,9 +10,9 @@ export const id = "Template";
  * Executed when a message with the corresponding
  * `id` is received
  *
- * @param {{webhook: WebhookWrapper, msg: string}} data Message Data
+ * @param {{webhook: WebhookWrapper, msg: string[]}} data Message Data
  */
 export async function execute({ webhook, msg }) {
-    const embed = EmbedWrapper.genericEmbed(msg);
+    const embed = EmbedWrapper.titleEmbed(msg.join("\n"));
     await webhook.sendEmbed(embed);
 }
